0c0d8e8297d158799e17c80c9b9708c3562f0abc
PauloAlbes8039 <pj38alves@gmail.com>
2021-07-11 21:06:06 -0300

    Refatorando frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,28 +1,11 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from "./views/home/home.component";
-import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
 
-import { ProductCreateComponent } from "./components/product/product-create/product-create.component";
-
-const routes: Routes = [
-  {
-    path: "",
-    component: HomeComponent,
-  },
-  {
-    path: "products",
-    component: ProductCrudComponent,
-  },
-  {
-    path: "products/create",
-    component: ProductCreateComponent,
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
