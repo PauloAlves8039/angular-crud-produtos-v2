9e193015bc856ae4671ffee3656b998bdee41262
PauloAlbes8039 <pj38alves@gmail.com>
2021-07-13 14:06:19 -0300

    Refatoração inicial do Product Create

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatListModule } from "@angular/material/list";
 import { HomeComponent } from "./views/home/home.component";
 import { MatCardModule } from "@angular/material/card";
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
+    ProductCreateComponent,
   ],
   imports: [
     BrowserModule,
