9ec533fdd34df8b31ba865ea3c70a0b720d540cc
PauloAlbes8039 <pj38alves@gmail.com>
2021-07-17 19:21:04 -0300

    Refatorando as implementações iniciais do componente Product Read

---
+++ frontend/src/app/componentes/product/product-read/product-read.component.ts
@@ -0,0 +1,21 @@
+import { ProductService } from "./../../../components/product/product.service";
+import { Product } from "./../../../components/product/product.model";
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-product-read",
+  templateUrl: "./product-read.component.html",
+  styleUrls: ["./product-read.component.css"],
+})
+export class ProductReadComponent implements OnInit {
+  products: Product[];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.read().subscribe((products) => {
+      this.products = products;
+      console.log(products);
+    });
+  }
+}
